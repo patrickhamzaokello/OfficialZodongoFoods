04e6f49ab7803a6f24cfa8040902e77678ec1cd7
pkasemer <pkasemer@gmail.com>
2020-09-29 23:40:37 +0300

    price for browse

--- lib/Browse.dart
+++ lib/Browse.dart
@@ -212,128 +212,133 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget _buildListItem(
-      String imgPath, String foodName, String price, String description) {
+    String imgPath,
+    String foodName,
+    String price,
+    String description,
+  ) {
     return InkWell(
-        onTap: () {
-          Navigator.of(context).push((MaterialPageRoute(
-              builder: (context) => DetailsPage(
-                    heroTag: imgPath,
-                    foodName: foodName,
-                    foodPrice: price,
-                    foodDescription: description,
-                  ))));
-        },
-        child: Padding(
-            padding: EdgeInsets.only(left: 20.0, top: 10.0, bottom: 4.0),
-            child: Container(
-                height: 200.0,
-                width: 200.0,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 6.0,
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 5.0)
-                    ]),
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                        height: 175.0,
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                colors: [Colors.white, Color(0xFFACBEA3)],
-                                begin: Alignment.topLeft,
-                                end: Alignment.bottomRight),
-                            borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(10.0),
-                                topRight: Radius.circular(10.0)))),
-                    Hero(
-                        tag: imgPath,
-                        child: Container(
-                          height: 175.0,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: AssetImage(imgPath),
-                                  fit: BoxFit.contain),
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(10.0),
-                                  topRight: Radius.circular(10.0))),
-                        )),
-                    Positioned(
-                        top: 160.0,
-                        right: 20.0,
-                        child: Material(
-                            elevation: 2.0,
-                            borderRadius: BorderRadius.circular(15.0),
-                            child: Container(
-                              height: 30.0,
-                              width: 30.0,
-                              child: Center(
-                                  child: Icon(Icons.favorite,
-                                      color: Colors.red, size: 17.0)),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15.0),
-                                  color: Colors.white),
-                            ))),
-                    Positioned(
-                        top: 190.0,
-                        left: 10.0,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(foodName,
-                                style: TextStyle(
-                                    fontFamily: 'Montserrat',
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.black,
-                                    fontSize: 14.0)),
-                            SizedBox(height: 3.0),
-                            Container(
-                                width: 175.0,
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: <Widget>[
-                                    Row(
-                                      children: <Widget>[
-                                        Text(
-                                          '4.9',
-                                          style: TextStyle(
-                                              fontFamily: 'Montserrat',
-                                              color: Colors.grey,
-                                              fontSize: 12.0),
-                                        ),
-                                        SizedBox(width: 3.0),
-                                        Icon(Icons.star,
-                                            color: Colors.green, size: 14.0),
-                                        Icon(Icons.star,
-                                            color: Colors.green, size: 14.0),
-                                        Icon(Icons.star,
-                                            color: Colors.green, size: 14.0),
-                                        Icon(Icons.star,
-                                            color: Colors.green, size: 14.0),
-                                        Icon(Icons.star,
-                                            color: Colors.green, size: 14.0),
-                                      ],
-                                    ),
-                                    Text(
-                                        'Ugx ' +
-                                            numformat.format(
-                                              int.parse(price),
-                                            ),
-                                        style: TextStyle(
-                                            fontFamily: 'Montserrat',
-                                            fontWeight: FontWeight.w500,
-                                            color: Colors.black,
-                                            fontSize: 13.0)),
-                                  ],
-                                ))
-                          ],
-                        ))
-                  ],
+      onTap: () {
+        Navigator.of(context).push((MaterialPageRoute(
+            builder: (context) => DetailsPage(
+                  heroTag: imgPath,
+                  foodName: foodName,
+                  foodPrice: price,
+                  foodDescription: description,
                 ))));
+      },
+      child: Padding(
+        padding: EdgeInsets.only(left: 20.0, top: 10.0, bottom: 4.0),
+        child: Container(
+          height: 200.0,
+          width: 200.0,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 6.0,
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 5.0)
+              ]),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                  height: 175.0,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.white, Color(0xFFACBEA3)],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10.0),
+                          topRight: Radius.circular(10.0)))),
+              Hero(
+                  tag: imgPath,
+                  child: Container(
+                    height: 175.0,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(imgPath), fit: BoxFit.contain),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10.0),
+                            topRight: Radius.circular(10.0))),
+                  )),
+              Positioned(
+                  top: 160.0,
+                  right: 20.0,
+                  child: Material(
+                      elevation: 2.0,
+                      borderRadius: BorderRadius.circular(15.0),
+                      child: Container(
+                        height: 30.0,
+                        width: 30.0,
+                        child: Center(
+                            child: Icon(Icons.favorite,
+                                color: Colors.red, size: 17.0)),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15.0),
+                            color: Colors.white),
+                      ))),
+              Positioned(
+                  top: 190.0,
+                  left: 10.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(foodName,
+                          style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                              fontSize: 14.0)),
+                      SizedBox(height: 3.0),
+                      Container(
+                          width: 175.0,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    '4.9',
+                                    style: TextStyle(
+                                        fontFamily: 'Montserrat',
+                                        color: Colors.grey,
+                                        fontSize: 12.0),
+                                  ),
+                                  SizedBox(width: 3.0),
+                                  Icon(Icons.star,
+                                      color: Colors.green, size: 14.0),
+                                  Icon(Icons.star,
+                                      color: Colors.green, size: 14.0),
+                                  Icon(Icons.star,
+                                      color: Colors.green, size: 14.0),
+                                  Icon(Icons.star,
+                                      color: Colors.green, size: 14.0),
+                                  Icon(Icons.star,
+                                      color: Colors.green, size: 14.0),
+                                ],
+                              ),
+                              Text(
+                                  'Ugx ' +
+                                      numformat.format(
+                                        int.parse(price),
+                                      ),
+                                  style: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.black,
+                                      fontSize: 13.0)),
+                            ],
+                          ))
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   List<int> totalprice = [];
@@ -407,6 +412,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       } else {
                         _cartList.remove(item);
                         totalprice.remove(totalpsrice);
+                        print("total $totalprice");
                       }
                     });
                   },
