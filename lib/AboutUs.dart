19091d587c0a6c175977fabe3a20bc140d5ccf15
pkasemer <pkasemer@gmail.com>
2020-10-09 13:22:23 +0300

    done

--- lib/AboutUs.dart
+++ lib/AboutUs.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutterloginui/brew_list.dart';
+import 'package:flutterloginui/services/database.dart';
+import 'package:provider/provider.dart';
 
 class AboutUs extends StatefulWidget {
   @override
@@ -8,9 +12,14 @@ class AboutUs extends StatefulWidget {
 class _AboutUsState extends State<AboutUs> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
         appBar: AppBar(
-      title: Text("About US"),
-    ));
+          title: Text("My Brew List"),
+        ),
+        body: BrewList(),
+      ),
+    );
   }
 }
