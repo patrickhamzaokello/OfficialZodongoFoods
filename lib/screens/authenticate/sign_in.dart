ee20cb99964b9aed4fd7691b550f6a741ddb265a
pkasemer <pkasemer@gmail.com>
2020-10-09 17:28:50 +0300

    new Login

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterloginui/services/auth.dart';
 import 'package:flutterloginui/shareds/constants.dart';
@@ -27,80 +27,171 @@ class _SignInState extends State<SignIn> {
     return loading
         ? Loading()
         : Scaffold(
-            backgroundColor: Colors.green[100],
+            // backgroundColor: Colors.white[100],
             appBar: AppBar(
               backgroundColor: Colors.green[400],
               elevation: 0.0,
-              title: Text('Sign In to Zodongo Foods'),
-              actions: <Widget>[
-                FlatButton.icon(
-                    onPressed: () async {
-                      widget.toggleView();
-                    },
-                    icon: Icon(Icons.person),
-                    label: Text('Register'))
-              ],
+              title: Text('Zodongo Foods'),
             ),
-            body: Container(
-                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                        decoration:
-                            textInputDecoration.copyWith(hintText: 'Email'),
-                        validator: (val) =>
-                            val.isEmpty ? 'Enter an Email' : null,
-                        onChanged: (val) {
-                          setState(() => email = val);
-                        },
-                      ),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                        decoration:
-                            textInputDecoration.copyWith(hintText: 'Password'),
-                        obscureText: true,
-                        validator: (val) => val.length < 6
-                            ? 'Enter a password 6+ chars long'
-                            : null,
-                        onChanged: (val) {
-                          setState(() => password = val);
-                        },
-                      ),
-                      SizedBox(height: 20.0),
-                      RaisedButton(
-                        color: Colors.pink[400],
-                        child: Text(
-                          'Sign In',
-                          style: TextStyle(color: Colors.white),
+            body: AnimatedContainer(
+              curve: Curves.fastLinearToSlowEaseIn,
+              duration: Duration(milliseconds: 1000),
+              child: Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                  child: Form(
+                    key: _formKey,
+                    child: ListView(
+                      children: <Widget>[
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'Email',
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 10)),
+                          validator: (val) =>
+                              val.isEmpty ? 'Enter an Email' : null,
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          },
                         ),
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            setState(() => loading = true);
+                        SizedBox(height: 20.0),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              hintText: 'Password',
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10, horizontal: 10)),
+                          obscureText: true,
+                          validator: (val) => val.length < 6
+                              ? 'Enter a password 6+ chars long'
+                              : null,
+                          onChanged: (val) {
+                            setState(() => password = val);
+                          },
+                        ),
+                        SizedBox(height: 20.0),
+                        Column(
+                          children: <Widget>[
+                            GestureDetector(
+                              onTap: () async {
+                                if (_formKey.currentState.validate()) {
+                                  setState(() => loading = true);
 
-                            dynamic result = await _auth
-                                .signInWithEmailAndPassword(email, password);
+                                  dynamic result =
+                                      await _auth.signInWithEmailAndPassword(
+                                          email, password);
 
-                            if (result == null) {
-                              setState(() {
-                                error =
-                                    'could not sign in with those credentials';
-                                loading = false;
-                              });
-                            }
-                          }
-                        },
-                      ),
-                      SizedBox(height: 12.0),
-                      Text(
-                        error,
-                        style: TextStyle(color: Colors.red, fontSize: 14.0),
-                      ),
-                    ],
-                  ),
-                )),
+                                  if (result == null) {
+                                    setState(() {
+                                      error =
+                                          'could not sign in with those credentials';
+                                      loading = false;
+                                    });
+                                  }
+                                }
+                              },
+                              child: PrimaryButton(
+                                btnText: "Sign In",
+                              ),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            GestureDetector(
+                              onTap: () async {
+                                widget.toggleView();
+                              },
+                              child: OutlineBtn(
+                                btnText: "Create Account",
+                              ),
+                            )
+                          ],
+                        ),
+                        // RaisedButton(
+                        //   color: Colors.pink[400],
+                        //   child: Text(
+                        //     'Sign In',
+                        //     style: TextStyle(color: Colors.white),
+                        //   ),
+                        //   onPressed: () async {
+                        //     if (_formKey.currentState.validate()) {
+                        //       setState(() => loading = true);
+
+                        //       dynamic result = await _auth
+                        //           .signInWithEmailAndPassword(email, password);
+
+                        //       if (result == null) {
+                        //         setState(() {
+                        //           error =
+                        //               'could not sign in with those credentials';
+                        //           loading = false;
+                        //         });
+                        //       }
+                        //     }
+                        //   },
+                        // ),
+                        SizedBox(height: 12.0),
+                        Text(
+                          error,
+                          style: TextStyle(color: Colors.red, fontSize: 14.0),
+                        ),
+                      ],
+                    ),
+                  )),
+            ),
           );
+  }
+}
+
+class PrimaryButton extends StatefulWidget {
+  final String btnText;
+  PrimaryButton({this.btnText});
+
+  @override
+  _PrimaryButtonState createState() => _PrimaryButtonState();
+}
+
+class _PrimaryButtonState extends State<PrimaryButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFF5AC035),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      padding: EdgeInsets.all(10),
+      child: Center(
+        child: Text(
+          widget.btnText,
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
+
+class OutlineBtn extends StatefulWidget {
+  final String btnText;
+  OutlineBtn({this.btnText});
+
+  @override
+  _OutlineBtnState createState() => _OutlineBtnState();
+}
+
+class _OutlineBtnState extends State<OutlineBtn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Color(0xFF5AC035), width: 2),
+          borderRadius: BorderRadius.circular(50)),
+      padding: EdgeInsets.all(10),
+      child: Center(
+        child: Text(
+          widget.btnText,
+          style: TextStyle(color: Color(0xFFB40284A), fontSize: 16),
+        ),
+      ),
+    );
   }
 }
